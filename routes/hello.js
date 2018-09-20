3fb92e55238be2bb6bf417a2510ce67a8b4afece
KidneyFlower <753475777@qq.com>
2018-09-20 11:44:13 +0800

    Add plugin joi

--- routes/hello.js
+++ routes/hello.js
@@ -1,25 +1,49 @@
+const Joi = require('joi')
+
 module.exports = [
   {
     method: 'GET',
     path: '/',
-    handler: async (request, h) => {
-      return 'Hello Hapi!'
-    },
     config: {
       tags: ['api', 'test'],
       description: '测试连接'
+    },
+    handler: async (request, h) => {
+      return 'Hello Hapi!'
     }
   },
   {
     method: 'GET',
     path: '/{name}',
+    config: {
+      tags: ['api', 'test'],
+      description: '测试动态路由',
+      validate: {
+        params: {
+          name: Joi.string().required().description('name')
+        }
+      }
+    },
     handler: async (request, h) => {
       const { name } = request.params
       return `Hello ${encodeURIComponent(name)}!`
-    },
+    }
+  },
+  {
+    method: 'POST',
+    path: '/post',
     config: {
       tags: ['api', 'test'],
-      description: '测试动态路由'
+      description: '测试POST',
+      validate: {
+        payload: {
+          name: Joi.string().required().description('name')
+        }
+      }
+    },
+    handler: async (request, h) => {
+      const { name } = request.payload
+      return `Hello ${name}!`
     }
   }
 ]
